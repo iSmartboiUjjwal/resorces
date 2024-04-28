478688fe8c26ee825d13ad296fe78c2d15aa1fb5
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2024-04-28 13:58:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,34 +2,43 @@ const hurl = "https://filestore-ismartboiujjwal.koyeb.app/watch/969";
 const videoUrl = hurl.replace("/watch/", "/dl/");
 
 
-function openExternalPlayer(playerName) {
-    const playerUrl = getPlayerUrl(playerName);
-    if (playerUrl) {
-        launchPlayer(playerUrl);
-    } else {
-        alert('Player not supported');
-    }
+function vlc() {
+    const nlink = videoUrl;
+    const vlc = `vlc://${nlink}`;
+    window.location.href = vlc;
 }
 
-function getPlayerUrl(playerName) {
-    switch (playerName) {
-        case 'MX Player':
-            return `intent:${videoUrl}#Intent;package=com.mxtech.videoplayer.ad;end`;
-        case 'VLC Player':
-            return `vlc://${videoUrl}`;
-        case 'PlayIt':
-            return `playit://playerv2/video?url=${videoUrl}`;
-        case 'SPlayer':
-            return `intent:${videoUrl}#Intent;action=com.young.simple.player.playback_online;package=com.young.simple.player;end`;
-        default:
-            return '';
-    }
+function mx() {
+    const nlink = videoUrl;
+    const openMx = `intent:${openbisallink}#Intent;package=com.mxtech.videoplayer.ad;end`;
+    window.location.href = openMx;
 }
 
-function launchPlayer(url) {
-    window.location.href = url;
+function playit() {
+    const nlink = videoUrl;
+    const pl = `playit://playerv2/video?url=${nlink}`;
+    window.location.href = pl;
 }
 
-function startFastDownload() {
-    window.location.href = videoUrl;
+function s() {
+    const nlink = videoUrl;
+    const sp = `intent:${nlink}#Intent;action=com.young.simple.player.playback_online;package=com.young.simple.player;end`;
+    window.location.href = sp;
+}
+
+function km() {
+    const nlink = videoUrl;
+    const km = `intent:${nlink}#Intent;package=com.kmplayer;end`;
+    window.location.href = km;
+}
+
+function Hd() {
+    const nlink = videoUrl;
+    const hd = `intent:${nlink}#Intent;package=uplayer.video.player;end`;
+    window.location.href = hd;
+}
+
+function dl() {
+    const nlink = videoUrl;
+    window.location.href = nlink;
 }
